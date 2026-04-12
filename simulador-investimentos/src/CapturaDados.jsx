be758539695a36973b939625ac856dca371c9325
Yasmin Feliciano <713yasminlima@gmail.com>
2026-04-11 22:01:01 -0300

    feat(p1): captura de dados do formulário

--- simulador-investimentos/src/CapturaDados.jsx
+++ simulador-investimentos/src/CapturaDados.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
-const CapturaDados = () => {
-  return (
-    <div>
-        <h3>Simulação</h3>
+const CapturaDados = (props) => {
+    const [inicial, setInicial] = useState(0);
+    const [aporte, setAporte] = useState(0);
+    const [taxa, setTaxa] = useState(0);
+    const [periodo, setPeriodo] = useState(0);
 
-        <input className="form-control mb-2" placeholder="Valor Inicial"/>
-        <input className="form-control mb-2" placeholder="Valor de Aporte"/>
-        <input className="form-control mb-2" placeholder="Taxa de Juros (%)"/>
-        <input className="form-control mb-2" placeholder="Período (meses)"/>
+    const enviar = () => {
+        props.setDados({
+            inicial: Number(inicial),
+            aporte: Number(aporte),
+            taxa: Number(taxa),
+            periodo: Number(periodo)
+        });
+    };
 
-        <button className="btn btn-primary w-100">Calcular</button>
-    </div>
-  );
-}
+    return (
+        <div>
+            <h3>Simulação</h3>
+
+            <input className="form-control mb-2" onChange={(e) =>setInicial(e.target.value)} placeholder="Valor Inicial"/>
+            <input className="form-control mb-2" onChange={(e) =>setAporte(e.target.value)} placeholder="Valor de Aporte"/>
+            <input className="form-control mb-2" onChange={(e) =>setTaxa(e.target.value)} placeholder="Taxa de Juros (%)"/>
+            <input className="form-control mb-2" onChange={(e) =>setPeriodo(e.target.value)} placeholder="Período (meses)"/>
+
+            <button className="btn btn-primary w-100" onClick={enviar}>Calcular</button>
+        </div>
+    );
+};
 export default CapturaDados;
