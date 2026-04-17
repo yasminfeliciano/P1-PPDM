caa6e3fb2aec5656412e348744e2d55f7d0ee8cd
Yasmin Feliciano <713yasminlima@gmail.com>
2026-04-16 21:56:18 -0300

    feat(p1): Adicionando o novo setHistorico com array e comentando

--- simulador-investimentos/src/App.jsx
+++ simulador-investimentos/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
     //guarda o resultado final da simulação
     const [resultado, setResultado] = useState(0);
     //guarda o histórico da simulação
-    const [historico, sethistorico] = useState([])
+    const [historico, setHistorico] = useState([])
 
     //função para receber os dados do formulário
     const calcular = (dados) => {
@@ -32,20 +32,47 @@ const App = () => {
             aportes: periodo
         });
 
-        /*sethistorico((prevHistorico) =>
+        //setHistorico com concat
+        /*setHistorico((prevHistorico) =>
+            //cria um novo array com um item novo na primeira posição
             [
                 {
                     valor: montante,
                     data:new Date().toLocaleString()
                 }
+            //concatena o novo array com o histórico anterior
             ].concat(prevHistorico)
         );*/
+
+        //setHistorico com Array
+        setHistorico((prevHistorico=[]) =>
+            {
+                //cria um array vazio para o novo historico
+                const novoHistorico = [];
+                                
+                //cria o novo item
+                const novoItem={
+                    valor:montante,
+                    data:new Date().toLocaleString()
+                }
+
+                //adiciona o novo item na primeira posição
+                novoHistorico[0] = novoItem;
+
+                //copia os itens antigos a partir da posição 1
+                for(let i=0;i<prevHistorico.length;i++){
+                    novoHistorico[i+1]=prevHistorico[i];
+                }
+
+                return novoHistorico;
+            }
+        );
     };
 
     //Limpa o historico
     const limpar = () =>{
         setResultado(0);
-        sethistorico([]);
+        setHistorico([]);
     }
 
     //passa a função calcular via props
